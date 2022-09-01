9a362c1c451d2ae46d0b3fe57c328feaa3dcb364
francesco <francesco.mazzola00@gmail.com>
2022-09-01 09:42:08 +0200

    added firs GET mehtod implementation inside game

--- blank-docker-project/web/frontend/views.py
+++ blank-docker-project/web/frontend/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from re import template
 
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.template import loader
+
+def gameplay(request):
+    template=loader.get_template("game.html")
+    return HttpResponse(template.render())
+
+def request(request):
+    pippo=request.GET.get("test")
+    return JsonResponse({"pippo": pippo})
