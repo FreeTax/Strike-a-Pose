77db453cfe5631910f3a11b5f4587f49e726fff7
Francesco Mazzola <francesco.mazzola00@gmail.com>
2022-09-03 14:21:59 +0200

    register and logou work

--- blank-docker-project/web/authenticator/urls.py
+++ blank-docker-project/web/authenticator/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^login', views.login_user, name='login'),
+    re_path(r'^logout', views.logout_user, name='logout'),
+    re_path(r'^register', views.register_user, name='register'),
 ]
