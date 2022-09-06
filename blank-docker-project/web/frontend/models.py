6122ee302be021080e376aaf6d484470db115baa
Francesco Mazzola <francesco.mazzola00@gmail.com>
2022-09-06 09:19:01 +0200

    added score db method and table

--- blank-docker-project/web/frontend/models.py
+++ blank-docker-project/web/frontend/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class User(models.Model):
@@ -53,10 +54,16 @@ class Video(models.Model):
     path = models.CharField(max_length=255, null=False)
     """user_id = models.Column(models.Integer, models.ForeignKey('user.id'),ullable=False)"""
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    #user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False)
 
     def as_dict(self):
         return {"id": self.id, "path": self.path}
 
+class Score(models.Model):
+    id=models.AutoField(primary_key=True)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    points=models.IntegerField(null=False)
+    date=models.DateTimeField(auto_now_add=True, null=True)
 """
 def insert_default_data():
     new_level = Level(
--- blank-docker-project/web/frontend/views.py
+++ blank-docker-project/web/frontend/views.py
@@ -4,11 +4,32 @@ from urllib.error import HTTPError
 from django.views.decorators import gzip
 from django.shortcuts import render
 from django.http import HttpResponse,StreamingHttpResponse,HttpResponseServerError
+from django.core import serializers
 import cv2
 from django.http import JsonResponse
+from .models import User, Level, Picture, Video, Score
 # Create your views here.
 
 def getImage(request):
     pippo=request.GET.get("test")
     return JsonResponse({"pippo": pippo})
+
+def setScore(request):
+    user_id = request.GET.get("user_id")
+    score=request.GET.get("score")
+
+    score= Score(user_id=user_id, points=score)
+    score.save()
+    return JsonResponse({"pippo": "pippo"})
+
+def getScoreByUser(request):
+    user_id = request.GET.get("user_id")
+    score=Score.objects.filter(user_id=user_id).order_by('-points')
+    data = serializers.serialize('json', score)
+    return HttpResponse(data, content_type='application/json')
+
+def getScore(request):
+    score=Score.objects.order_by('-points')
+    data = serializers.serialize('json', score)
+    return HttpResponse(data, content_type='application/json')
 
