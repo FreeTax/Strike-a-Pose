c4e81d4565bab3fc50bfdba37d14080f296e47e8
Francesco Mazzola <francesco.mazzola00@gmail.com>
2022-09-15 11:34:22 +0200

    settiings.py edit for https compatibilty

--- blank-docker-project/web/blank/settings.py
+++ blank-docker-project/web/blank/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5eph3^9&fo_))q2bko^xsh_&-af*@)44y8$v5zk5qx+0!o4d$b'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://francescomazzola.com', 'https://www.francescomazzola.com', 'http://francescomazzola.com', 'http://www.francescomazzola.com', 'http://localhost:8000']
 
 # Application definition
 
