9523226ef4dfb8aca55d1b452db9bb501a413743
Marco <trambusti.marco23@gmail.com>
2022-09-15 13:54:06 +0200

    bugfix

--- blank-docker-project/web/blank/settings.py
+++ blank-docker-project/web/blank/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'blank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blank',
-        'USER':'root',
-        'PASSWORD': 'root',
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
